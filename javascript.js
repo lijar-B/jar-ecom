9a4a2cd04953289f6ce9975d004ce3a7f077fb8e
beljar <davidbeljar@gmail.com>
2022-01-16 10:23:19 +0630

    ad

--- javascript.js
+++ javascript.js
@@ -21,17 +21,17 @@ function show(shown, hidden) {
 }
 
 // fixed top when it comes to top
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-          document.getElementById('categoryBar').classList.add('fixed-bottom');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.category-bar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('categoryBar').classList.remove('fixed-bottom');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
+// document.addEventListener("DOMContentLoaded", function(){
+//     window.addEventListener('scroll', function() {
+//         if (window.scrollY > 50) {
+//           document.getElementById('categoryBar').classList.add('fixed-bottom');
+//           // add padding top to show content behind navbar
+//           navbar_height = document.querySelector('.category-bar').offsetHeight;
+//           document.body.style.paddingTop = navbar_height + 'px';
+//         } else {
+//           document.getElementById('categoryBar').classList.remove('fixed-bottom');
+//            // remove padding top from body
+//           document.body.style.paddingTop = '0';
+//         } 
+//     });
+//   }); 
